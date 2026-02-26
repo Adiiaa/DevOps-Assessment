23e43d82979b5354ed89930c27b3d4c00744b01a
AdiaUwase <adia.uwase24snhu@keplercollege.ac.rw>
2026-02-26 14:11:54 +0200

    docs: changing the person to greet in test.java file

---
+++ src/test/java/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static String greet(String name) {
+        return "Hello, " + name + "! Welcome to the DevOps.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(greet("Sam john"));
+    }
+}
